cd939829a51b21a83460f128ebb738fd14d035aa
lioneljones <lioneljones5116@gmail.com>
2022-06-03 14:41:28 -0500

    finished up routes

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div style={{marginTop:200}}>
+       <main>
+         Login
+       </main>
+    </div>
+  )
+}
+
+export default Login
